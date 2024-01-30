8256e30c9fc8ba3c14edf20eb7bb424862ebc2ae
jeonghyeon <3575253@naver.com>
2024-01-30 15:54:19 +0900

    cra 기본

--- react-fro-beginners/src/App.js
+++ react-fro-beginners/src/App.js
@@ -1,9 +1,10 @@
-
+import Button from "./Button";
+import styles from "./App.module.css";
 function App() {
   return (
-    <div >
-      <h1>hi..</h1>
-
+    <div>
+      <h1 className={styles.title}>hiddd..</h1>
+      <Button text={"zzzz"} />
     </div>
   );
 }
