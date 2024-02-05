179545203b42f9ceb28092fff0612c3beae80565
jeonghyeon <3575253@naver.com>
2024-02-05 10:16:03 +0900

    movie app

--- react-fro-beginners/src/App.js
+++ react-fro-beginners/src/App.js
@@ -1,6 +1,15 @@
-import Coin from "./Coin.js";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import MovieHome from "./movie/routes/MovieHomejs";
 function App() {
-  return <Coin />;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/">
+          <MovieHome />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
